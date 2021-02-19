a8d16829a2f49befdb617d7682f6e9dbf7f1aea0
gllms <willemvanderdoe@hotmail.com>
2021-02-19 21:59:08 +0100

    Add support for brackets at the start of the line

--- cursedFormatter.js
+++ cursedFormatter.js
@@ -1,27 +1,42 @@
-let chars = "{};"                                              ;
-function format(text)                                          {
-  let lines = text.split("\n")                                 ;
-  let longest = Math.max(...(lines.map(line => line.length)))  ;
-  let ends = []                                                ;
-  let fixedLines = []                                          ;
-  let j = 0                                                    ;
-  lines.forEach((line, i) =>                                   {
-    while (true)                                               {
-      let last = line.substr(-1, 1)                            ;
-      if (chars.includes(last) && last != "")                  {
-        ends[i] = last + (ends[i] || "")                       ;
-        line = line.slice(0, -1)                               ;}
-      else if (last == " ")                                    {
-        line = line.slice(0, -1)                               ;}
-      else break                                               ;}
-    if (/^ *$/.test(line) && i > 0)                            {
-      ends[j] = (ends[j] || "") + (ends[i] || "")              ;}
-    else                                                       {
-      fixedLines[i] = line                                     ;
-      j = i                                                    ;}
-  })                                                           ;
-  let result = ""                                              ;
-  fixedLines.forEach((e, i) =>                                 {
-    result += e.padEnd(longest + 1) + (ends[i] || "") + "\n"   ;
-  })                                                           ;
-  document.querySelector("#output").innerHTML = result         ;}
+let chars = "{};"                                                  ;
+function format(text)                                              {
+  let lines = text.split("\n")                                     ;
+  let ends = new Array(lines.length).fill("")                      ;
+  let fixedLines = new Array(lines.length).fill("")                ;
+  let j = 0                                                        ;
+  lines.forEach((line, i) =>                                       {
+    while (true)                                                   {
+      let last = line.substr(-1, 1)                                ;
+      if (chars.includes(last) && last != "")                      {
+        ends[i] = last + ends[i] || ""                             ;
+        line = line.slice(0, -1)                                   ;}
+      else if (last == " ")                                        {
+        line = line.slice(0, -1)                                   ;}
+      else break                                                   ;}
+    let k = 0                                                      ;
+    let f = false                                                  ;
+    while (k < line.length)                                        {
+      let cur = line[k]                                            ;
+      if (cur == " ")                                              {
+        if (f) line = line.slice(0, k) + line.slice(k + 1)         ;
+        else                                                       {
+          k++                                                      ;
+          continue                                                 ;}}
+      else if (chars.includes(cur) && typeof (cur) != "undefined") {
+        ends[j] = (ends[j] || "") + cur                            ;
+        line = line.slice(0, k) + line.slice(k + 1)                ;
+        f = true                                                   ;}
+      else break                                                   ;}
+    if (/^ *$/.test(line) && i > 0)                                {
+      ends[j] = ends[j] + ends[i]                                  ;
+      ends[i] = ""                                                 ;}
+    else                                                           {
+      fixedLines[i] = line                                         ;
+      j = i                                                        ;}}
+  )                                                                ;
+  let longest = Math.max(...(fixedLines.map(line => line.length))) ;
+  let result = ""                                                  ;
+  fixedLines.forEach((e, i) =>                                     {
+    if (ends[i]) result += e.padEnd(longest + 1) + ends[i] + "\n"  ;}
+  )                                                                ;
+  document.querySelector("#output").innerHTML = result             ;}
